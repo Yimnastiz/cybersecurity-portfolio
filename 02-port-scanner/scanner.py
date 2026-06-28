0f9317f613af3e3bd914e8e386c133b1267b29ae
Yimnastiz <otakuyim1150@gmail.com>
2026-06-28 12:58:50 +0700

    Refactor port scanner script for improved readability and structure

--- 02-port-scanner/scanner.py
+++ 02-port-scanner/scanner.py
@@ -1,3 +1,14 @@
 import socket
-target = input("Target IP :")
+target = input("Target IP : ")
 port = int(input("Port : "))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+result = sock.connect_ex((target, port))
+
+if result == 0:
+    print("Port is OPEN")
+else:
+    print("Port is CLOSED")
+
+sock.settimeout(1)
