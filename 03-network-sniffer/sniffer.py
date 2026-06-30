ab3eec6105cdb1569a2864cd6a2641017f79b069
Yimnastiz <otakuyim1150@gmail.com>
2026-06-30 15:06:23 +0700

    fix(sniffer): correct syntax error in socket creation

--- 03-network-sniffer/sniffer.py
+++ 03-network-sniffer/sniffer.py
@@ -4,12 +4,12 @@ def main():
 
     sock = socket.socket(
         socket.AF_PACKET,
-        socket.SOCK_RAW<
+        socket.SOCK_RAW,
         socket.ntohs(3)
     )
 
     host = input("Local IP Address: ")
-    
+
     print("Waiting for packet . . .\n")
 
     packet = sock.recvfrom(65535)
